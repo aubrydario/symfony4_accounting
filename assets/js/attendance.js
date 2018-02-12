cdcc65ca8c470efe77617e46813d4aaf4a03b268
Dario Aubry <Dario Aubry@globonet.internal>
2018-02-12 16:53:46 +0100

    get data from api & dry code

--- assets/js/attendance.js
+++ assets/js/attendance.js
@@ -1,77 +1,108 @@
 import moment from 'moment';
+import d3 from 'd3';
 
-const data = [{
-    'Date': '2018-02-06',
-    'Name': 'Dario Aubry',
-    'Abo': '10er-Abo'
-}, {
-    'Date': '2018-02-08',
-    'Name': 'Luca Aubry',
-    'Abo': 'Einzelstunde'
-}, {
-    'Date': '2018-02-12',
-    'Name': 'Gianni Aubry',
-    'Abo': 'Einzelstunde'
-}, {
-    'Date': '2018-02-10',
-    'Name': 'Beatrice Aubry',
-    'Abo': '10er-Abo'
-}];
+/*const data = [
+    {
+        'Date': '2018-02-06',
+        'Name': 'Dario Aubry',
+        'Abo': '10er-Abo'
+    },
+    {
+        'Date': '2018-02-13',
+        'Name': 'Luca Aubry',
+        'Abo': 'Einzelstunde'
+    },
+    {
+        'Date': '2018-02-16',
+        'Name': 'Gianni Aubry',
+        'Abo': 'Einzelstunde'
+    },
+    {
+        'Date': '2018-02-10',
+        'Name': 'Beatrice Aubry',
+        'Abo': '10er-Abo'
+    }
+];*/
 
-const table = d3.select('body').append('table').attr('class', 'table table-bordered');
+$.ajax({
+    type:'GET',
+    url:'http://localhost:8000/api/attendance',
+    dataType: 'json',
+    async: true,
+    contentType: "application/json; charset=utf-8"
+}).done(data => {
+    createTable(data);
+});
+
+const table = d3.select('#attendance-table').append('table').attr('class', 'table table-bordered');
 const thead = table.append('thead');
 const tbody = table.append('tbody');
 
-function tabulate(data) {
-    // append the header row
+function createTable(data) {// append the header row
     thead.append('tr')
         .append('th')
         .text('Name');
 
-    for (let i = 0; i < 14; i++) {
+    for (let i = 0; i < 15; i++) {
         thead.select('tr')
             .append('th')
             .text(moment().add(i, 'days').format('D.M.YY'));
     }
 
     // create a row for each object in the data
-    const rows = tbody.selectAll('tr')
+    tbody.selectAll('tr')
         .data(data)
         .enter()
         .append('tr');
 
-    for (let i = 1; i <= data.length; i++) {
-        let j = i - 1;
+    data.forEach((item, i) => {
         const row = tbody.select(`tr:nth-child(${i})`);
-        row.append('td')
-            .text(data[j].Name);
+        let itemDate = moment(item.date).format('YYYY-MM-DD');
 
-        for (let k = 1; k <= 14; k++) {
+        row.append('td')
+            .text(`${item.firstname} ${item.surname}`);
+
+        for (let j = 1; j <= 15; j++) {
+            let theadDate = moment(thead.selectAll('th')[0][j].innerHTML, 'D.M.YY').format('YYYY-MM-DD');
+
             row.append('td');
 
-            if (data[j].Abo === 'Einzelstunde') {
-                if (moment(data[j].Date).format('YYYY-MM-DD') === moment(thead.selectAll('th')[0][k].innerHTML, 'D.M.YY').format('YYYY-MM-DD')) {
-                    let field = row.select(`td:nth-child(${thead.selectAll('th')[0][k].cellIndex + 1})`)
-                        .attr('class', 'einzelstunde');
+            switch(item.abo_id) {
+                //Einzelstunde
+                case '1':
+                    if (itemDate === theadDate) {
+                        let field = row.select(`td:nth-child(${thead.selectAll('th')[0][j].cellIndex + 1})`)
+                            .attr('class', 'einzelstunde');
 
-                    field.on('click', () => { showInfo(k, 'Einzelstunde'); });
-                }
-            }
+                        field.on('click', () => { showInfo(theadDate, 'Einzelstunde'); });
+                    }
+                    break;
 
-            if (data[j].Abo === '10er-Abo') {
-                if (moment(data[j].Date).format('YYYY-MM-DD') <= moment(thead.selectAll('th')[0][k].innerHTML, 'D.M.YY').format('YYYY-MM-DD') &&
-                    moment(data[j].Date).add(12, 'weeks').format('YYYY-MM-DD') >= moment(thead.selectAll('th')[0][k].innerHTML, 'D.M.YYYY').format('YYYY-MM-DD')) {
-                    let field = row.select(`td:nth-child(${thead.selectAll('th')[0][k].cellIndex + 1})`)
-                        .attr('class', 'zehnerabo');
+                //Schnupperstunde
+                case '2':
+                    console.log('Schnupperstunde');
+                    break;
 
-                    field.on('click', () => { showInfo(k, '10er-Abo'); });
-                }
+                //10er-Abo
+                case '3':
+                    if (itemDate <= theadDate && moment(item.date).add(12, 'weeks').format('YYYY-MM-DD') >= theadDate) {
+                        let field = row.select(`td:nth-child(${thead.selectAll('th')[0][j].cellIndex + 1})`)
+                            .attr('class', 'zehnerabo');
+
+                        field.on('click', () => { showInfo(theadDate, '10er-Abo'); });
+                    }
+                    break;
+
+                //Jahresabo
+                case '4':
+                    console.log('Jahresabo');
+                    break;
             }
         }
-    }
+    });
 }
 
-function showInfo(k, abo) {
+function showInfo(date, abo) {
     if(!d3.event.target.hasChildNodes()) {
         let icon = d3.event.target.appendChild(document.createElement('i'));
         icon.classList.add('fa');
@@ -79,12 +110,9 @@ function showInfo(k, abo) {
     }
 
     let response = {
-        date: moment(thead.selectAll('th')[0][k].innerHTML, 'D.M.YYYY').format('YYYY-MM-DD'),
+        date: date,
         name: d3.event.target.parentNode.firstChild.innerHTML,
         abo: abo
     };
     console.log(response);
 }
-
-// render the table
-tabulate(data);
