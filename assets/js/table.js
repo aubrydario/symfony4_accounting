853e85f6906d0c53e88dd3bf1948258941150140
Dario Aubry <dario.aubry@gmail.com>
2018-06-19 08:54:38 +0200

    bill & payment success-message

--- assets/js/table.js
+++ assets/js/table.js
@@ -13,6 +13,8 @@ function modal(uri) {
     });
 
     document.getElementById('modalDeleteLink').addEventListener('click', () => {
+        const elSuccessMessage = document.getElementById('success-message');
+
         if(uri === '/customers') {
             ajax('PUT', `/api${uri}/${modalLink.getAttribute('data-id')}`, {
                 data: JSON.stringify({active: 0}),
@@ -20,15 +22,25 @@ function modal(uri) {
                     $(`#delete${modalId.slice(0, modalId.length - 1)}Modal`).hide();
                     $('.modal-backdrop').hide();
 
-                    document.getElementById('success-message').innerHTML = 'Kunde deaktiviert.';
-                    document.getElementById('success-message').style.display = 'inline-block';
+                    elSuccessMessage.innerHTML = 'Kunde deaktiviert.';
+                    elSuccessMessage.style.display = 'inline-block';
                 }
             });
         } else {
-            ajax('DELETE', `/api${uri}s/${modalLink.getAttribute('data-id')}`);
+            ajax('DELETE', `/api${uri}s/${modalLink.getAttribute('data-id')}`, {
+                complete: () => {
+                    if(modalId === 'Bill') {
+                        elSuccessMessage.innerHTML = 'Abonement erfolgreich gelöscht.';
+                    } else {
+                        elSuccessMessage.innerHTML = 'Zahlung erfolgreich gelöscht.';
+                    }
 
-            $(`#delete${modalId}Modal`).hide();
-            $('.modal-backdrop').hide();
+                    elSuccessMessage.style.display = 'inline-block';
+
+                    $(`#delete${modalId}Modal`).hide();
+                    $('.modal-backdrop').hide();
+                }
+            });
         }
     });
 }
