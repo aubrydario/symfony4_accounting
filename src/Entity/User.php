2ca9225247e663a03b850d81012bb44cb9d3ed17
Dario Aubry <dario.aubry@gmail.com>
2018-07-06 11:37:41 +0200

    fos user-bundle setup

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,9 +4,9 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * @ApiResource()
@@ -14,147 +14,18 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields="username", message="Benutzername bereits vergeben")
  */
-class User implements UserInterface, \Serializable
+class User extends BaseUser
 {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $firstname;
-
-    /**
-     * @ORM\Column(type="string", length=50)
-     */
-    private $surname;
-
-    /**
-     * @ORM\Column(type="string", length=50, unique=true)
-     * @Assert\NotBlank()
-     */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=64)
-     * @Assert\NotBlank()
-     */
-    private $password;
-
-    /**
-     * @Assert\Length(max=4096)
-     * @Assert\NotBlank()
-     */
-    private $plainPassword;
-
-    /**
-     * @ORM\Column(name="email", type="string", length=100, nullable=true)
-     * @Assert\NotBlank()
-     */
-    private $email;
-
-    public function getId()
+    public function __construct()
     {
-        return $this->id;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getSurname(): ?string
-    {
-        return $this->surname;
-    }
-
-    public function setSurname(string $surname): self
-    {
-        $this->surname = $surname;
-
-        return $this;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getPlainPassword2()
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword($password)
-    {
-        $this->plainPassword = $password;
-    }
-
-    public function getEmail(): ?string {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): void {
-        $this->email = $email;
-    }
-
-    public function getSalt() {
-        return null;
-    }
-
-    public function getRoles() {
-        return array('ROLE_USER');
-    }
-
-    public function eraseCredentials() {
-
-    }
-
-    /** @see \Serializable::serialize() */
-    public function serialize() {
-        return serialize(array(
-            $this->id,
-            $this->username,
-            $this->password
-        ));
-    }
-
-    /** @see \Serializable::unserialize() */
-    public function unserialize($serialized) {
-        list (
-            $this->id,
-            $this->username,
-            $this->password
-        ) = unserialize($serialized, ['allowed_classes' => false]);
+        parent::__construct();
+        // your own logic
     }
 }
