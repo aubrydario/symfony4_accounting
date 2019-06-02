4bcd93d43201b4a4a8dec207d3be1e8f364a9c48
aubrydario <dario.aubry@gmail.com>
2019-06-02 16:41:26 +0200

    react sidebar

--- assets/js/components/listItem.jsx
+++ assets/js/components/listItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class ListItem extends React.Component{
     render() {
         return (
-            <li className="mdc-list-item"><a className="mdc-list-item__text" href={"/" + this.props.href}>{this.props.text}</a></li>
+            <li className="sidebar-menu-item"><a className="mdc-list-item__text" href={"/" + this.props.href}>{this.props.text}</a></li>
         );
     }
 }
