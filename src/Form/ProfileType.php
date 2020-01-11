3725bbc47426631d5e80cff5caaba6636bf18d09
aubrydario <dario.aubry@gmail.com>
2020-01-11 21:13:44 +0100

    add firstname & lastname to profile + can edit

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname')
+          ->add('lastname');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
